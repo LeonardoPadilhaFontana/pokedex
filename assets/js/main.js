96132e2115eb6502bed9766346bc5fb9e484bd0b
Leonardo Padilha Fontana <leonardopad2@gmail.com>
2024-07-26 23:42:29 -0300

    Adicao de funcao para carregar os dados da api separado do main.js e iniciado processo de insercao da lista no html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemontoLi(pokemon){
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="${pokemon.name}">
+                </div>
+            
+            </li>
+            `
+}
 
-fetch(url)
-    .then((response) => response .json())
-    .then((jsonBody) => console.log(jsonBody))
+const pokemonList = document.getElementById('pokemonList');
+    pokeApi.getPokemons().then((pokemons = []) =>{
+        pokemonList.innerHTML += pokemons.map(convertPokemontoLi).join('');
+    })
     .catch((error)   => console.log(error))
